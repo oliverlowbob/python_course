ba7c877a1e1e75fa2a3b4c62968f7e4ca9767c3a
oliverlowbob <oliv605m@stud.kea.dk>
2020-03-03 10:13:10 +0100

    der må dabbes

--- week10/bank.py
+++ week10/bank.py
@@ -6,8 +6,11 @@ class Bank:
     def __repr__(self):
         return str(self.__dict__)
 
+
+
 class Account:
-    def __init__(self, name, balance):
+    def __init__(self, accountNumber, name, balance):
+        self.accountNumber = accountNumber
         self.name = name
         self.balance = balance
         self.customers = {}
@@ -16,7 +19,7 @@ class Account:
         return str(self.__dict__)
 
 class Customer:
-    def __inint__(self, *args):
+    def __init__(self, *args):
         if len(args) == 1:
             self.name = args[0] 
         if len(args) == 2:
@@ -25,6 +28,17 @@ class Customer:
             
     def __repr__(self):
         return str(self.__dict__)
-        
+
 class DanskeBank:
     pass
+
+bank = Bank("Den Helt Dyre")
+customer1 = Customer("Dabmaster 5000", "1337-420-69")
+account1 = Account("1", customer1, 500)
+bank.accounts[customer1] = account1
+
+customer2 = Customer("Freddy Licious", "12349-1241")
+account2 = Account("2", customer1, 500)
+bank.accounts[customer2] = account2
+
+print(bank.accounts)
